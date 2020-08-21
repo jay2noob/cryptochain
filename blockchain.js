3f5a51d898072744f55290a56c81d7bf562177f3
John Fields <jjfields24@outlook.com>
2020-08-20 21:27:49 -0400

    created test for preventing difficulty jumps

--- blockchain.js
+++ blockchain.js
@@ -37,6 +37,7 @@ class Blockchain {
     for (let i = 1; i < chain.length; i++) {
       const { timestamp, lastHash, hash, nonce, difficulty, data } = chain[i];
       const actualLastHash = chain[i - 1].hash;
+      const lastDifficulty = chain[i - 1].difficulty;
 
       if (lastHash !== actualLastHash) return false;
 
@@ -49,6 +50,8 @@ class Blockchain {
       );
 
       if (hash !== validatedHash) return false;
+
+      if (lastDifficulty - difficulty > 1) return false;
     }
 
     return true;
--- blockchain.test.js
+++ blockchain.test.js
@@ -1,5 +1,6 @@
 const Blockchain = require("./blockchain");
 const Block = require("./block");
+const cryptoHash = require("./crypto-hash");
 
 describe("Blockchain", () => {
   let blockchain, newChain, originalChain;
@@ -66,6 +67,19 @@ describe("Blockchain", () => {
           const nonce = 0;
           const data = [];
           const difficulty = lastBlock.difficulty - 3;
+          const hash = cryptoHash(timestamp, lastHash, difficulty, nonce, data);
+          const badBlock = new Block({
+            timestamp,
+            lastHash,
+            hash,
+            nonce,
+            difficulty,
+            data,
+          });
+
+          blockchain.chain.push(badBlock);
+
+          expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
         });
       });
 
