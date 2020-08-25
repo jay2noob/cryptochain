6bbeebfd3fb7bcab4b2e8ba058ed25770c99d155
John Fields <jjfields24@outlook.com>
2020-08-24 21:17:53 -0400

    installed body-parser and created post request in express

--- index.js
+++ index.js
@@ -8,5 +8,13 @@ app.get("/api/blocks", (req, res) => {
   res.json(blockchain.chain);
 });
 
+app.post("/api/mine", (req, res) => {
+  const { data } = req.body;
+
+  blockchain.addBlock({ data });
+
+  res.redirect("/api/blocks");
+});
+
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Listening at localhost:${PORT}`));
