b8586b0bac0c538f7ce307141a21968a1d753f5f
John Fields <jjfields24@outlook.com>
2020-09-10 12:00:36 -0400

    created syncChains

--- index.js
+++ index.js
@@ -29,6 +29,20 @@ app.post("/api/mine", (req, res) => {
   res.redirect("/api/blocks");
 });
 
+const syncChains = () => {
+  request(
+    { url: `${ROOT_NODE_ADDRESS}/api/blocks` },
+    (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        const rootChain = JSON.parse(body);
+
+        console.log("replace chain on a sync with", rootChain);
+        blockchain.replaceChain(rootChain);
+      }
+    }
+  );
+};
+
 let PEER_PORT;
 
 if (process.env.GENERATE_PEER_PORT === "true") {
@@ -36,4 +50,8 @@ if (process.env.GENERATE_PEER_PORT === "true") {
 }
 
 const PORT = PEER_PORT || DEFAULT_PORT;
-app.listen(PORT, () => console.log(`Listening at localhost:${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Listening at localhost:${PORT}`);
+
+  syncChains();
+});
