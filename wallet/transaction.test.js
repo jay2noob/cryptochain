01b59913176ed40ee9ebb055d7684a18e8736590
John Fields <jjfields24@outlook.com>
2020-09-30 16:02:55 -0400

    validTransaction() test

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -60,4 +60,30 @@ describe("Transaction", () => {
       ).toBe(true);
     });
   });
+
+  describe("validTransaction()", () => {
+    describe("when the transaction is valid", () => {
+      it("returns true", () => {
+        expect(Transaction.validTransaction(transaction)).toBe(true);
+      });
+    });
+
+    describe("when the transaction is invalid", () => {
+      describe("and a transaction outputMap is invalid", () => {
+        it("returns false", () => {
+          transaction.outputMap[senderWallet.publicKey] = 999999;
+
+          expect(Transaction.validTransaction(transaction)).toBe(false);
+        });
+      });
+
+      describe("and the transaction input signature is invalid", () => {
+        it("returns false", () => {
+          transaction.input.signature = new Wallet().sign("data");
+
+          expect(Transaction.validTransaction(transaction)).toBe(false);
+        });
+      });
+    });
+  });
 });
