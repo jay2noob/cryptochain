a56885b3d29b2bfbbd7517c2b1c53d923119d12f
John Fields <jjfields24@outlook.com>
2020-10-07 06:05:42 -0400

    fixed errors to pass tests

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -28,10 +28,14 @@ class Transaction {
 
   update({ senderWallet, recipient, amount }) {
     if (amount > this.outputMap[senderWallet.publicKey]) {
-      throw new Error("Amount exceeds the balance");
+      throw new Error("Amount exceeds balance");
     }
 
-    this.outputMap[recipient] = amount;
+    if (!this.outputMap[recipient]) {
+      this.outputMap[recipient] = amount;
+    } else {
+      this.outputMap[recipient] = this.outputMap[recipient] + amount;
+    }
 
     this.outputMap[senderWallet.publicKey] =
       this.outputMap[senderWallet.publicKey] - amount;
