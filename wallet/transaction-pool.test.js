ec628b47e360fc058e71bdad0bad7ad0f54af193
John Fields <jjfields24@outlook.com>
2020-10-10 19:57:16 -0400

    created dynamic variables

--- wallet/transaction-pool.test.js
+++ wallet/transaction-pool.test.js
@@ -1,3 +1,7 @@
 const TransactionPool = require("./transaction-pool");
 const Transaction = require("./transaction");
 const Wallet = require("./index");
+
+describe("TransactionPool", () => {
+  let transactionPool, transaction;
+});
