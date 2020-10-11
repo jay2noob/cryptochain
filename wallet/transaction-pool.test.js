6aa9790f874b8178d20e392c35505854eb5c6f17
John Fields <jjfields24@outlook.com>
2020-10-11 12:31:38 -0400

    made beforeEach function

--- wallet/transaction-pool.test.js
+++ wallet/transaction-pool.test.js
@@ -4,4 +4,13 @@ const Wallet = require("./index");
 
 describe("TransactionPool", () => {
   let transactionPool, transaction;
+
+  beforeEach(() => {
+    transactionPool = new TransactionPool();
+    transaction = new Transaction({
+      senderWallet: new Wallet(),
+      recipient: "fake-recipient",
+      amount: 50,
+    });
+  });
 });
