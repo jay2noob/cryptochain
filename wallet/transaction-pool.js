1871d4520fd1b04d02711b8c7d0d1b8d813ebd90
John Fields <jjfields24@outlook.com>
2020-10-13 12:54:01 -0400

    created setTransaction() method

--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -2,6 +2,10 @@ class TransactionPool {
     constructor() {
         this.transactionMap = {};
     }
+
+    setTransaction(transaction) {
+        this.transactionMap[transaction.id] = transaction;
+    }
 }
 
 module.exports = TransactionPool;
